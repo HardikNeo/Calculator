94a110d68744c7f0283993c434342ceeca85ea20
Hardik <neopanehardik630@gmail.com>
2024-04-12 00:21:31 +0545

    fifth commit: made the calculator fully functional

--- main.js
+++ main.js
@@ -1,30 +1,55 @@
-
 var op = "";
-var a = 0;  
-function ope() {
+var a = 0;
+var num1 = 0;
+var num2 = 0;
+var res = 0;
+var check = 0;
+const predis = document.getElementById("pre-dis");
+const resdis = document.getElementById("res-dis");
+function num(b) {
+  if (check === 0) {
+    predis.textContent = "";
+    resdis.textContent = "";
+    check = 1;
+  }
+  a = a * 10 + b;
+  predis.textContent += b;
+}
+function ope(operator) {
   num1 = a;
   a = 0;
+  op = operator;
+  predis.textContent += " " + op + " ";
 }
+
 function eq() {
   num2 = a;
   console.log(num1, num2);
   a = 0;
-  if op ="+"{
+  if (op === "+") {
     res = num1 + num2;
-  }else if op ="-"{
+  } else if (op === "-") {
     res = num1 - num2;
-  }else if op ="/"{
+  } else if (op === "/") {
     res = num1 / num2;
-  }else if op ="*"{
+  } else if (op === "*") {
     res = num1 * num2;
-  }else {
-    console.log("IDK WHAT HAPPENED")
-  } 
+  } else {
+    console.log("IDK WHAT HAPPENED");
+  }
   console.log(res);
+  resdis.textContent = "= " + res;
+  check = 0;
+  a = 0;
+  res = 0;
+  num1 = 0;
+  num2 = 0;
 }
-function ac(){
-  a =0;
-  num1=0;
-  num2=0;
-  res=0; 
+function ac() {
+  a = 0;
+  num1 = 0;
+  num2 = 0;
+  res = 0;
+  predis.textContent = "";
+  resdis.textContent = "";
 }
